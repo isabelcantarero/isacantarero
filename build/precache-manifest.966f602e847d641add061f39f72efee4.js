69999532c2701c5a99aa9fb9055f92cc6886712d
Isabel <isacantarero1@gmail.com>
2020-05-06 18:25:03 -0500

    GraciasHiromi

---
+++ build/precache-manifest.966f602e847d641add061f39f72efee4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0e51e51ec4989660f0f302bcbec3781",
+    "url": "/isabelcantarero/isacantarero/index.html"
+  },
+  {
+    "revision": "c2c93e8a30ccd2aa885d",
+    "url": "/isabelcantarero/isacantarero/static/css/main.52e83449.chunk.css"
+  },
+  {
+    "revision": "144365a274f23c334ef3",
+    "url": "/isabelcantarero/isacantarero/static/js/2.3cbf5f71.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/isabelcantarero/isacantarero/static/js/2.3cbf5f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c93e8a30ccd2aa885d",
+    "url": "/isabelcantarero/isacantarero/static/js/main.1f2783c6.chunk.js"
+  },
+  {
+    "revision": "6775458e328c37e7ad3d",
+    "url": "/isabelcantarero/isacantarero/static/js/runtime-main.49bf695b.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/isabelcantarero/isacantarero/static/media/logo.ee7cd8ed.svg"
+  }
+]);
