e970abbabffab683f688e2bb4271f1aa59aa29f1
Robert <robert>
2020-06-03 20:15:27 -0500

    Updates

---
+++ precache-manifest.195e5abe070e0c0729d25d24ebf15131.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228ae965e94638e2845a4564634740a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33a5b993b4efc83ff531",
+    "url": "/static/css/main.6bac7f8e.chunk.css"
+  },
+  {
+    "revision": "37f89f716d3eefec0c0d",
+    "url": "/static/js/2.0d454177.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.0d454177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33a5b993b4efc83ff531",
+    "url": "/static/js/main.ddf9ea2c.chunk.js"
+  },
+  {
+    "revision": "418a170f585be95bc71b",
+    "url": "/static/js/runtime-main.68601e82.js"
+  }
+]);
