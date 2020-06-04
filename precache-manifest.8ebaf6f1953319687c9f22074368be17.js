e3f2a340b0ac4ed27c9b758627376d2fa34795ca
Robert <robert>
2020-06-03 20:25:44 -0500

    Updates

---
+++ precache-manifest.8ebaf6f1953319687c9f22074368be17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f70284232271457fcff53a36b870d958",
+    "url": "/isacantarero/index.html"
+  },
+  {
+    "revision": "ae817b49995969be5086",
+    "url": "/isacantarero/static/css/main.6bac7f8e.chunk.css"
+  },
+  {
+    "revision": "37f89f716d3eefec0c0d",
+    "url": "/isacantarero/static/js/2.0d454177.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/isacantarero/static/js/2.0d454177.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae817b49995969be5086",
+    "url": "/isacantarero/static/js/main.3652259b.chunk.js"
+  },
+  {
+    "revision": "1505b4791b63941c8378",
+    "url": "/isacantarero/static/js/runtime-main.173e597d.js"
+  }
+]);
