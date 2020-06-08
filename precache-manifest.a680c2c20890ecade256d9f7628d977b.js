ee2c1db110c12eefbf78f58719a729d771a60897
Isabel <isacantarero1@gmail.com>
2020-06-07 19:59:23 -0500

    Updates

---
+++ precache-manifest.a680c2c20890ecade256d9f7628d977b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f8c138b9d0d23c83d4d7161a2e91c5",
+    "url": "/isacantarero/index.html"
+  },
+  {
+    "revision": "31a793e0d8e8441700cb",
+    "url": "/isacantarero/static/css/main.6bac7f8e.chunk.css"
+  },
+  {
+    "revision": "9018653081a39b433996",
+    "url": "/isacantarero/static/js/2.bb83664e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/isacantarero/static/js/2.bb83664e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31a793e0d8e8441700cb",
+    "url": "/isacantarero/static/js/main.b6619dc8.chunk.js"
+  },
+  {
+    "revision": "1505b4791b63941c8378",
+    "url": "/isacantarero/static/js/runtime-main.173e597d.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/isacantarero/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2c32b20194b59b752972bf5c737b5e6e.js"
+  "/isacantarero/precache-manifest.a680c2c20890ecade256d9f7628d977b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/isacantarero/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
